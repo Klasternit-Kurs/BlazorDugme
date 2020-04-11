112a04cfd5a14859c1c5e3068412dc176cded0a3
Dragan Pepic <d.pepic@hotmail.com>
2020-04-11 19:38:59 +0200

    Add project files.

---
+++ BlazorApp4/Server/Hab.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorApp4.Server
+{
+	public class Hab : Hub
+	{
+		public void Foo()
+		{
+			Console.WriteLine("Ping");
+			Clients.Caller.SendAsync("NekaMetoda", "od haba with love :) ");
+		}
+
+		public void Bar(string s)
+		{
+			Console.WriteLine(s);
+		}
+	}
+}
